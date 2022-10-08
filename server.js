e00f4ecfb93ee727f21214a0b08520a8d6317fd2
uiop5487 <uiop5487@gmail.com>
2022-10-08 13:30:27 +0900

    2022-10-08

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
   return res.send("hello word");
 });
 
+app.post("users", (req, res) => {
+  const user = req.body.user;
+
+  res.send(user);
+});
+
 app.listen(3000, () => {
   console.log("sever start!");
 });
